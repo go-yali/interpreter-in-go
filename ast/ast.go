be20eb34352fe1dac177cacaa4585b4e4a737c6e
ritabc <ritabc@gmail.com>
2019-03-08 10:14:29 -0500

    Add string methods helpful for debugging and testing

--- ast/ast.go
+++ ast/ast.go
@@ -2,13 +2,17 @@
 
 package ast
 
-import "monkey/token"
+import (
+	"bytes"
+	"monkey/token"
+)
 
 // Each of our AST Nodes (ie each expression, statement) all must implement the Node interface, aka it must provide a TokenLiteral() that returns the literal value of the token it's associated with. TokenLiteral will only be used for debugging and testing
 // All nodes will be connected to each other
 // Some nodes implement the Statement interface, some the expression interface
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement interface only contains a dummy method, statementNode()
@@ -67,4 +71,54 @@ func (ls *LetStatement) TokenLiteral() string {
 
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
+}
+
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+// String method of Program only creates a buffer and writes the return value of each statement's String() method to it
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return ex.Expression.String()
+	}
+	return ""
+}
+
+func (i *Identifier) String() {
+	return i.Value
 }
