961887dfbfc4f1b0b76a85e1508d3a5044b9c6d7
Rita Bennett-Chew <ritabc@gmail.com>
2019-03-28 13:38:25 -0400

    Add failing tests for if else conditionals

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -68,7 +68,7 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}
 }
 
-/// BANG ///
+/// BANG OPERATOR ///
 func TestBangOperator(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -85,6 +85,32 @@ func TestBangOperator(t *testing.T) {
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
 		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+/// IF ELSE ///
+func TestIfElseExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{"if (true) { 10 }", 10},
+		{"if (false) { 10 }", nil},
+		{"if (1) { 10 }", 10},
+		{"if (1 < 2) { 10 }", 10},
+		{"if (1 > 2) { 10 }", nil},
+		{"if (1 > 2) { 10 } else { 20 }", 20},
+		{"if (1 < 2) { 10 } else { 20 }", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
 	}
 }
 
@@ -125,5 +151,13 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 		return false
 	}
 
+	return true
+}
+
+func testNullObject(t *testing.T, obj object.Object) bool {
+	if obj != NULL {
+		t.Errorf("object is not NULL. got=%T (%v)", obj, obj)
+		return false
+	}
 	return true
 }
