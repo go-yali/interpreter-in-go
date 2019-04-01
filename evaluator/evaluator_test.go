ed9d438e2f3f4642a27d82231de9d0c0c7336d71
Rita Bennett-Chew <ritabc@gmail.com>
2019-04-01 16:59:23 -0400

    Add closures test

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -272,6 +272,17 @@ func TestFunctionApplication(t *testing.T) {
 	}
 }
 
+func TestClosures(t *testing.T) {
+	input := `
+		let newAdder = fn(x) {
+			fn(y) {x + y};
+		};
+		let addTwo = newAdder(2);
+		addTwo(2);
+		`
+	testIntegerObject(t, testEval(input), 4)
+}
+
 /// HELPERS ///
 func testEval(input string) object.Object {
 	l := lexer.New(input)
