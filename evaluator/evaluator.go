fe0c320e2a276fc812f2365da5c497d3284fb2fc
Rita Bennett-Chew <ritabc@gmail.com>
2019-04-01 16:16:52 -0400

    Test for function object, pass

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -38,6 +38,10 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return val
 		}
 		env.Set(node.Name.Value, val)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
 
 	// Expressions
 	case *ast.IntegerLiteral:
--- object/object.go
+++ object/object.go
@@ -1,6 +1,11 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"monkey/ast"
+	"strings"
+)
 
 type ObjectType string
 
@@ -10,6 +15,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 type Object interface {
@@ -35,14 +41,35 @@ type Error struct {
 	Message string
 }
 
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
 func (i *Integer) Type() ObjectType      { return INTEGER_OBJ }
 func (b *Boolean) Type() ObjectType      { return BOOLEAN_OBJ }
 func (n *Null) Type() ObjectType         { return NULL_OBJ }
 func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 func (e *Error) Type() ObjectType        { return ERROR_OBJ }
+func (f *Function) Type() ObjectType     { return FUNCTION_OBJ }
 
 func (i *Integer) Inspect() string      { return fmt.Sprintf("%d", i.Value) }
 func (b *Boolean) Inspect() string      { return fmt.Sprintf("%t", b.Value) }
 func (n *Null) Inspect() string         { return "null" }
 func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
 func (e *Error) Inspect() string        { return "ERROR: " + e.Message }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+	return out.String()
+}
